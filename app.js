e9859f37c2615ee516f2312cc49343bd5e596eed
Braulio Castillo <brauliocastillofernandez@gmail.com>
2025-09-07 08:49:57 -0400

    Agregada funcion de lista de amigos

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 console.log("prueba de conexión");
 let listaAmigos = [];
+
+function asignarTextoElemento(elemento,texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function agregarAmigo(){
+//funcion para agregar los amigos
+let nuevoAmigo = document.getElementById('amigo').value;
+if (nuevoAmigo == ""){
+        alert("Escribe un nombre por favor");
+    } 
+else {
+        listaAmigos.push(nuevoAmigo);
+        limpiarCaja();
+        console.log(listaAmigos);
+
+
+
+    }
+}
+
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
+    return;
+}
