5a9871f468a260ed02da47ad9aeb316a8b9680c8
Braulio Castillo <brauliocastillofernandez@gmail.com>
2025-09-07 17:50:14 -0400

    agregada funcion que modifica la lista

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 console.log("prueba de conexión");
 let listaAmigos = [];
+let miLista = document.getElementById('listaAmigos');
 
 function asignarTextoElemento(elemento,texto){
     let elementoHTML = document.querySelector(elemento);
@@ -16,11 +17,9 @@ if (nuevoAmigo == ""){
     } 
 else {
         listaAmigos.push(nuevoAmigo);
+        agregaLista();
         limpiarCaja();
-        console.log(listaAmigos);
-
-
-
+        console.log(listaAmigos); //para probar que funciona
     }
 }
 
@@ -28,4 +27,15 @@ else {
 function limpiarCaja(){
     document.querySelector('#amigo').value = '';
     return;
+}
+
+function agregaLista(){
+
+    miLista.innerHTML = '';
+    for (let i=0; i< listaAmigos.length;i++){
+        let nuevoLi = document.createElement('li');
+        nuevoLi.textContent=listaAmigos[i];
+        miLista.appendChild(nuevoLi);
+
+    }
 }
